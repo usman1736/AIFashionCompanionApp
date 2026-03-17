a987f42a36b4ff8d6703177ccfbda6e2c8ca3635
Fatima1515 <fatima.elhawi4@gmail.com>
2026-03-16 18:20:13 -0600

    Add Firebase configuration and initialization

---
+++ firebaseConfig.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFVx0D0t2bRylsccLU4JzYe-boGZZLPGQ",
+  authDomain: "aura-fashion-app-70d22.firebaseapp.com",
+  projectId: "aura-fashion-app-70d22",
+  storageBucket: "aura-fashion-app-70d22.firebasestorage.app",
+  messagingSenderId: "916868527893",
+  appId: "1:916868527893:web:626dd0735fc103f6a2511c",
+  measurementId: "G-9BJS36WBTP"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
